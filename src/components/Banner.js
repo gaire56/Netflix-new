bde1585b7075827835f52f39bfe9acf396f4d71e
Anant Gaire <56.anant@gmail.com>
2021-02-04 11:52:53 +0545

    banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,10 +2,34 @@ import React from 'react';
 import './Banner.css';
 
 function Banner() {
+  function truncate(string, n) {
+    return string?.length > n ? string.substr(0, n - 1) + '...' : string;
+  }
+
   return (
-    <div className="banner">
-      <h1>hi from bannder</h1>
-    </div>
+    <header
+      className="banner"
+      style={{
+        backgroundSize: 'cover',
+        backgroundImage: `url("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png")`,
+        backgroundPosition: 'center center',
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">Movie Name</h1>
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+        <h1 className="banner__description">
+          {truncate(
+            `This Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock,  test`,
+            150
+          )}
+        </h1>
+      </div>
+      <div cla="banner--fadeBottom" />
+    </header>
   );
 }
 
