516c5d37875d061b6572c5b32154cb6c69f3fca3
Anant Gaire <56.anant@gmail.com>
2021-02-04 09:43:26 +0545

    nav create

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import HomeScreen from './components/HomeScreen';
 
 function App() {
   return (
     <div className="app">
-      <h1>Hi from app</h1>
+      <HomeScreen />
     </div>
   );
 }
---
+++ src/components/HomeScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './HomeScreen.css';
+import Nav from './Nav';
+
+function HomeScreen() {
+  return (
+    <div className="homeScreen">
+      {/* NavBar */}
+      <Nav />
+
+      {/* Bannder */}
+
+      {/* Row */}
+    </div>
+  );
+}
+
+export default HomeScreen;
