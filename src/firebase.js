827e934c3b7bbc8e7c0aff9d3640f091a732c907
Anant Gaire <56.anant@gmail.com>
2021-02-04 09:06:02 +0545

    firebase save

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyDGyDJmNwxk94iBICz28IgwOohCSQyePlo',
+  authDomain: 'netflix-4331b.firebaseapp.com',
+  projectId: 'netflix-4331b',
+  storageBucket: 'netflix-4331b.appspot.com',
+  messagingSenderId: '295301046522',
+  appId: '1:295301046522:web:7fdcf0695b4362b238a943',
+  measurementId: 'G-ZZY3TJ080W',
+};
