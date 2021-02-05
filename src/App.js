2b00318e7599d64ee2784da37e4802a53f83fe38
Anant Gaire <56.anant@gmail.com>
2021-02-05 11:58:57 +0545

    import login logout

--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ import HomeScreen from './screens/HomeScreen';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import LoginScreen from './screens/LoginScreen';
 import { auth } from './firebase/firebase';
+import { useDispatch } from 'react-redux';
+import { login, logout } from './features/userSlice';
 
 function App() {
   const user = null;
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((userAuth) => {
       if (userAuth) {
         //Logged in
         console.log(userAuth);
+        dispatch(login);
       } else {
         //Logged out
+        dispatch(logout);
       }
     });
 
