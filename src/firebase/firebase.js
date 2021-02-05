9f704a8601215476ce95a048c7a4db0fbd8221a0
Anant Gaire <56.anant@gmail.com>
2021-02-05 11:09:40 +0545

    firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
   apiKey: 'AIzaSyDGyDJmNwxk94iBICz28IgwOohCSQyePlo',
   authDomain: 'netflix-4331b.firebaseapp.com',
@@ -7,3 +9,10 @@ const firebaseConfig = {
   appId: '1:295301046522:web:7fdcf0695b4362b238a943',
   measurementId: 'G-ZZY3TJ080W',
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
