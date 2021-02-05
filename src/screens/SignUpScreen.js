c83dcaa6b4d5faac0e31be86126184fec5626f38
Anant Gaire <56.anant@gmail.com>
2021-02-05 11:21:02 +0545

    user add

--- src/screens/SignUpScreen.js
+++ src/screens/SignUpScreen.js
@@ -1,4 +1,5 @@
 import React, { useRef } from 'react';
+import { auth } from '../firebase/firebase';
 import './SignUpScreen.css';
 
 function SignUpScreen() {
@@ -7,6 +8,18 @@ function SignUpScreen() {
 
   const register = (e) => {
     e.preventDefault();
+
+    auth
+      .createUserWithEmailAndPassword(
+        emailRef.current.value,
+        passwordRef.current.value
+      )
+      .then((authUser) => {
+        console.log(authUser);
+      })
+      .catch((error) => {
+        alert(error.message);
+      });
   };
 
   const signIn = (e) => {
@@ -17,8 +30,8 @@ function SignUpScreen() {
     <div className="signupScreen">
       <form>
         <h1>Sign In</h1>
-        <input type="email" placeholder="Email" />
-        <input type="password" placeholder="Password" />
+        <input ref={emailRef} type="email" placeholder="Email" />
+        <input ref={passwordRef} type="password" placeholder="Password" />
         <button type="submit" onClick={signIn}>
           Sign In
         </button>
